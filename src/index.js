20236551402323d16ce18cdd43a0f53ed1830588
Isadora Ávila <isadorgravila@gmail.com>
2022-07-06 18:47:47 -0300

    Produtos - get /products e get /products/:id

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import productRouter from './routers/productRouter.js';
 
 dotenv.config();
 
@@ -8,6 +9,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(productRouter);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Servidor funcionando na porta ${PORT}`));
